17d734ef8aba48e39312177f7f95d5e005302bad
LuvaiCutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2021-01-17 16:14:10 +0000

    Working on custom LayoutManager

--- BalloonGame.java
+++ BalloonGame.java
@@ -1,7 +1,9 @@
 package miscallaneous;
 
-import java.awt.Component;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,49 +16,74 @@ import javax.swing.JPanel;
 public class BalloonGame {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Frame");
-		JPanel panel = new JPanel();
-		frame.setSize(800, 800);
-		frame.setLayout(new FlowLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		panel.add(createClickableBalloon(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				
-			}
-		}));
-		frame.add(panel);
+		JFrame frame = createMainWindow();
+		JPanel panel = createGamePanel();
+		Balloon balloon = Balloon.createBalloon(frame, panel);
+		balloon.attachBalloon(panel);
+		frame.add(panel, BorderLayout.CENTER);
+		frame.pack();
 		frame.setVisible(true);
 	}
 	
-	private static JButton createClickableBalloon(ActionListener listener) {
-		Icon icn = new ImageIcon("C:\\Users\\luvaidc\\Pictures\\balloon.jpg");
-		JButton button = new JButton(icn);
-		button.addActionListener(listener);
-		button.setSize(50, 50);
-		button.setOpaque(true);
-		button.setVisible(true);
-		return button;
+	static JFrame createMainWindow() {
+		JFrame frame = new JFrame("Balloon Game");
+		frame.setSize(800, 800);
+		frame.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return frame;
+	}
+	
+	static JPanel createGamePanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(10, 10));
+		panel.setPreferredSize(new Dimension(800, 800));
+		panel.setBackground(Color.DARK_GRAY);
+		return panel;
 	}
 }
 
-class Balloon extends Component implements ActionListener{
+class GameLayout {
 	
-	static final long serialVersionUID = 0l;
+	private final int BUTTON_WIDTH = 50;
+	private final int BUTTON_HEIGHT = 50;
 	
-	private JPanel parent;
+	public GameLayout(JFrame frame) {
+		
+	}
+	
+	private void setMainWindowListener(JFrame frame) {
+		
+	}
+	
+}
+
+class Balloon {
+	
 	private JButton button;
 	
-	private Balloon(JPanel parent, JButton button) {
-		this.parent = parent;
+	private Balloon(JButton button) {
 		this.button = button;
 	}
 	
-	public void actionPerformed(ActionEvent event) {
-		parent.remove(this);
+	void attachBalloon(JPanel panel) {
+		panel.add(this.button);
 	}
 	
-	static Balloon balloonFactory(JPanel parent) {
-		return new Balloon(parent, new JButton());
+	static Balloon createBalloon(JFrame master, JPanel parent) {
+		Icon icn = new ImageIcon("C:\\Users\\luvaidc\\Pictures\\balloon.jpg");
+		JButton button = new JButton(icn);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				master.setVisible(false);
+				parent.remove(button);
+				master.setVisible(true);
+			}
+		});
+		button.setBackground(Color.DARK_GRAY);
+		button.setSize(50, 50);
+		button.setOpaque(true);
+		button.setVisible(true);
+		return new Balloon(button);
 	}
 	
 }
